dafd055384b85c68633cc898d22c696341711982
z-maker <zero66694@gmail.com>
2019-02-16 23:29:21 -0600

    estilos y material design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Card,CardText} from 'material-ui/Card';
+
+import {blue400,orangeA400,lightBlueA400,amberA400} from 'material-ui/styles/colors';
 
 import Title from './components/Title'
-import Iron from './components/iron/Iron'
-import {TitleSub} from './components/Title'
+
+import './App.css';
 
 class App extends Component {
 
@@ -13,11 +16,10 @@ constructor(props){
   this.state = {
     numero:0
   };
-
-
+  this.updateNumero = this.updateNumero.bind(this);
 }
 
-updateNumer(){
+updateNumero(){
   this.setState({
     numero : this.state.numero + 1
   });
@@ -25,32 +27,43 @@ updateNumer(){
 
  render() {
     return (
-     <section>
-      <div>
-      <div>
-        <Title></Title>
-        <button onClick={()=>{this.updateNumer()}}>Crear cuenta</button>
-        <h2>{this.state.numero}</h2>
-        <img src={process.env.PUBLIC_URL + '/images/top-bg.png'} width="200"/>
-        <div>
-          <ul>
-            <li>
-              <h3>Calificanos con emociones</h3>
-              <p>Califica tus lugares con experiencias,no con numeros</p>
-            </li>
-            <li>
-              <h3>Sin Internet?</h3>
-              <p>Places funciona sin Internet</p>
-            </li>
-            <li>
-              <h3>Tus lugares favoritos</h3>
-              <p>Guarda tus lugares favoritos</p>
-            </li>
-          </ul>
+     <MuiThemeProvider >
+      <div className="Header-background">
+      <div style={{"width":"80%","margin":"0 auto"}}>
+        <div className="Header-main">
+          <Title></Title>
+          <RaisedButton onClick={this.updateNumero} label="Crear cuenta" secondary={true}/>
+          <h2>{this.state.numero}</h2>
+          <img className="Header-ilustration" src={process.env.PUBLIC_URL + '/images/top-bg.png'} />
         </div>
       </div>
       </div>
-     </section>
+      <div style={{'background-color':blue400,'padding':'50px'}}>
+        <ul>
+        <Card className="Header-Benefit">
+          <CardText style={{'backgroundColor':orangeA400}}>
+            <img src={process.env.PUBLIC_URL + 'images/heart.png'} />
+            <h3>Calificanos con emociones</h3>
+            <p>Califica tus lugares con experiencias,no con numeros</p>
+          </CardText>
+        </Card>
+        <Card className="Header-Benefit">
+          <CardText style={{'backgroundColor':orangeA400}}>
+            <img src={process.env.PUBLIC_URL + 'images/no-internet.png'} />
+            <h3>Sin Internet?</h3>
+            <p>Places funciona sin Internet</p>
+          </CardText>
+        </Card>
+        <Card className="Header-Benefit">
+          <CardText style={{'backgroundColor':orangeA400}}>
+            <img src={process.env.PUBLIC_URL + 'images/heart.png'} />
+            <h3>Tus lugares favoritos</h3>
+            <p>Guarda tus lugares favoritos</p>
+          </CardText>
+        </Card>
+        </ul>
+      </div>
+     </MuiThemeProvider>
     );
   }
 }

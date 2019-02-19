9bbb8c16d33f3faec23bbe0680f827584ecd9108
z-maker <zero66694@gmail.com>
2019-02-18 23:22:36 -0600

    Refactor and places

---
+++ src/request/places.js
@@ -0,0 +1,19 @@
+export default{
+	places:[
+	{
+		imageUrl:'/images/places-1.jpg',
+		title:'Silent Hill',
+		description:'Ven si te atreves...'
+	},
+	{
+		imageUrl:'/images/places-2.jpg',
+		title:'Bikini Bottom',
+		description:'Esponja de espera...'
+	},
+	{
+		imageUrl:'/images/places-3.jpg',
+		title:'Springfield',
+		description:'Hay caramba!!...'
+	}
+	]
+}
